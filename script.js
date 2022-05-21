66af7f1b5b0bc1f1a5e9068e15098773d3ef502b
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2022-05-21 18:01:17 -0300

    :construction: logical structure
    creating logical structure for generate number

---
+++ script.js
@@ -0,0 +1,14 @@
+function generate(){
+ var n1 = Math.floor(Math.random() * 60)
+ var n2 = Math.floor(Math.random() * 60)
+ var n3 = Math.floor(Math.random() * 60)
+ var n4 = Math.floor(Math.random() * 60)
+ var n5 = Math.floor(Math.random() * 60)
+ var n6 = Math.floor(Math.random() * 60)
+
+ if(n1,n2,n3,n4,n5,n6 !== n1,n2,n3,n4,n5,n6){
+  console.log(n1,n2,n3,n4,n5,n6)
+ }else{
+  console.log("dublicat number")
+ };
+}
