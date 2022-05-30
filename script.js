62d56251311c15fbafcf2de49134e8e7c6171ef0
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2022-05-29 21:26:44 -0300

    :construction: create New Element
    added new elemente in HTML with generated numbers

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function generate(){
+
     const numbers = [...Array(6).keys()].map((_) =>  Math.floor(Math.random() * 60 + 1));
     const unicNumbers = new Set(numbers);
+
+    const newElement = document.createElement('div');
+    newElement.className = "newNumber";
+    
+    const selElement = document.querySelector('.megaSema');
+    selElement.appendChild(newElement)
+
     if (unicNumbers.size !== numbers.length) {
-      console.log('you have dublicatees', unicNumbers, numbers)
+      console.log('you have dublicatees', unicNumbers, numbers);
       return;
      }
+    newElement.innerHTML = `<h1>${numbers}</h1>`;
     console.log('All is cool', unicNumbers, numbers)
     return;
   }
