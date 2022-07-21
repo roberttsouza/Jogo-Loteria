116754f737b87ba74234c9033c79586e1e2ccf74
Robertt Souza <48107814+roberttsouza@users.noreply.github.com>
2022-07-21 17:31:20 -0300

    Alteração no código :construction:
    Realizado a alteração dos códigos que geram os números randômicos parta um código mais clean.

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-function generateSena(){
-
-    const numbers = [...Array(6).keys()].map((_) =>  Math.floor(Math.random() * 60 + 1));
-    const unicNumbers = new Set(numbers);
-
-    const newElement = document.createElement('div');
-    newElement.className = "newNumber";
-    
-    const selElement = document.querySelector('.megaSema');
-    selElement.appendChild(newElement)
-
-    if (unicNumbers.size !== numbers.length) {
-      console.log('you have dublicatees', unicNumbers, numbers);
-      return;
-     }
-    newElement.innerHTML = `<h1>${numbers}</h1>`;
-    console.log('All is cool', unicNumbers, numbers)
-    return;
-  };
+function generateSena(max,quant) {
+  var numeroS = []
+  while (numeroS.length < 6) {
+    e = Math.ceil(Math.random() * 60);
+    if (numeroS.indexOf(e) == -1) {
+      numeroS.push(e)
+    }
+  }
+  const newElement = document.createElement('div');
+  newElement.className = "newNumber";
+  
+  const selElement = document.querySelector('.megaSema');
+  selElement.appendChild(newElement);
 
 
+  newElement.innerHTML = `<h1>${numeroS}</h1>`;
+  console.log(numeroS)
+  return;
+};
 
 
-  function generateFacil(){
 
-    const numbers = [...Array(15).keys()].map((_) =>  Math.floor(Math.random() * 25 + 1));
-    const unicNumbers = new Set(numbers);
 
+function generateFacil(max,quant) {
+    var numeroS = []
+    while (numeroS.length < 15) {
+      e = Math.ceil(Math.random() * 25);
+      if (numeroS.indexOf(e) == -1) {
+        numeroS.push(e)
+      }
+    }
     const newElement = document.createElement('div');
     newElement.className = "newNumber";
     
     const selElement = document.querySelector('.lotoFacil');
-    selElement.appendChild(newElement)
+    selElement.appendChild(newElement);
 
-    if (unicNumbers.size !== numbers.length) {
-      console.log('you have dublicatees', unicNumbers, numbers);
-      return;
-     }
-    newElement.innerHTML = `<h1>${numbers}</h1>`;
-    console.log('All is cool', unicNumbers, numbers)
+
+    newElement.innerHTML = `<h1>${numeroS}</h1>`;
+    console.log(numeroS)
     return;
-  };
+};
 
 
 
-  function generateQuina(){
-
-    const numbers = [...Array(5).keys()].map((_) =>  Math.floor(Math.random() * 80 + 1));
-    const unicNumbers = new Set(numbers);
-
+  function generateQuina(max,quant) {
+    var numeroS = []
+    while (numeroS.length < 5) {
+      e = Math.ceil(Math.random() * 80);
+      if (numeroS.indexOf(e) == -1) {
+        numeroS.push(e)
+      }
+    }
     const newElement = document.createElement('div');
     newElement.className = "newNumber";
     
     const selElement = document.querySelector('.quina');
-    selElement.appendChild(newElement)
+    selElement.appendChild(newElement);
 
-    if (unicNumbers.size !== numbers.length) {
-      console.log('you have dublicatees', unicNumbers, numbers);
-      return;
-     }
-    newElement.innerHTML = `<h1>${numbers}</h1>`;
-    console.log('All is cool', unicNumbers, numbers)
+
+    newElement.innerHTML = `<h1>${numeroS}</h1>`;
+    console.log(numeroS)
     return;
-  }
+};
