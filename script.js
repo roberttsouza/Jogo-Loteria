f52cdf6bec16aca4623968e43e9ac881df1309bd
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2022-08-20 18:24:01 -0300

    :construction: add btn de apagar
    incrementado um botão com a função de apagar uma sequencia de números gerado pela aplicação

--- script.js
+++ script.js
@@ -1,64 +1,89 @@
-function generateSena(max,quant) {
-  var numeroS = []
+function generateSena(max, quant) {
+  var numeroS = [];
   while (numeroS.length < 6) {
     e = Math.ceil(Math.random() * 60);
     if (numeroS.indexOf(e) == -1) {
-      numeroS.push(e)
+      numeroS.push(e);
     }
   }
   const newElement = document.createElement('div');
-  newElement.className = "newNumber";
-  
+  newElement.className = 'newNumber';
+
   const selElement = document.querySelector('.containerNewNumberMega');
   selElement.appendChild(newElement);
 
+  newElement.innerHTML = `<button class="close" type="submit">&times;</button>
+                          <h3>${numeroS}</h3>`;
 
-  newElement.innerHTML = `<h3>${numeroS}</h3>`;
-  console.log(numeroS)
-  return;
-};
+  //botão para excluir a task desejada
+  const close_btns = document.querySelectorAll('.close'); //criar variavel com a clase informada
 
+  close_btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.parentElement.style.display = 'none'; //crindo uma função de evendo para modificar o display da clase selecionada para none. isso não     exclui o elemento, somente deixa ele oculto.
 
+      console.log(numeroS);
+      return;
+    });
+  });
+}
 
-
-function generateFacil(max,quant) {
-    var numeroS = []
-    while (numeroS.length < 15) {
-      e = Math.ceil(Math.random() * 25);
-      if (numeroS.indexOf(e) == -1) {
-        numeroS.push(e)
-      }
+function generateFacil(max, quant) {
+  var numeroS = [];
+  while (numeroS.length < 15) {
+    e = Math.ceil(Math.random() * 25);
+    if (numeroS.indexOf(e) == -1) {
+      numeroS.push(e);
     }
-    const newElement = document.createElement('div');
-    newElement.className = "newNumber";
-    
-    const selElement = document.querySelector('.containerNewNumberFacil');
-    selElement.appendChild(newElement);
+  }
+  const newElement = document.createElement('div');
+  newElement.className = 'newNumber';
 
+  const selElement = document.querySelector('.containerNewNumberFacil');
+  selElement.appendChild(newElement);
 
-    newElement.innerHTML = `<h3>${numeroS}</h3>`;
-    console.log(numeroS)
-    return;
-};
+  newElement.innerHTML = `<button class="close" type="submit">&times;</button>
+                          <h3>${numeroS}</h3>`;
 
+  //botão para excluir a task desejada
+  const close_btns = document.querySelectorAll('.close'); //criar variavel com a clase informada
 
+  close_btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.parentElement.style.display = 'none'; //crindo uma função de evendo para modificar o display da clase selecionada para none. isso não     exclui o elemento, somente deixa ele oculto.
 
-  function generateQuina(max,quant) {
-    var numeroS = []
-    while (numeroS.length < 5) {
-      e = Math.ceil(Math.random() * 80);
-      if (numeroS.indexOf(e) == -1) {
-        numeroS.push(e)
-      }
+      console.log(numeroS);
+      return;
+    });
+  });
+}
+
+function generateQuina(max, quant) {
+  var numeroS = [];
+  while (numeroS.length < 5) {
+    e = Math.ceil(Math.random() * 80);
+    if (numeroS.indexOf(e) == -1) {
+      numeroS.push(e);
     }
-    const newElement = document.createElement('div');
-    newElement.className = "newNumber";
-    
-    const selElement = document.querySelector('.containerNewNumberQuina');
-    selElement.appendChild(newElement);
+  }
+  const newElement = document.createElement('div');
+  newElement.className = 'newNumber';
 
+  const selElement = document.querySelector('.containerNewNumberQuina');
+  selElement.appendChild(newElement);
 
-    newElement.innerHTML = `<h3>${numeroS}</h3>`;
-    console.log(numeroS)
-    return;
-};
+  newElement.innerHTML = `<button class="close" type="submit">&times;</button>
+                          <h3>${numeroS}</h3>`;
+
+  //botão para excluir a task desejada
+  const close_btns = document.querySelectorAll('.close'); //criar variavel com a clase informada
+
+  close_btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.parentElement.style.display = 'none'; //crindo uma função de evendo para modificar o display da clase selecionada para none. isso não     exclui o elemento, somente deixa ele oculto.
+
+      console.log(numeroS);
+      return;
+    });
+  });
+}
